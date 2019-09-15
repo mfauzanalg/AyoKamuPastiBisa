514e5e073cc4cd149d369deebcfcfdee871330c8
mfauzanalg <53439112+mfauzanalg@users.noreply.github.com>
2019-09-15 08:49:27 +0700

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 class hello {
   public static void main(String[ ] args) {
-    System.out.println("HelloWol  asd");
+    System.out.println("Hello My name is Fauzan Yesh");
     System.out.println("Hello");
+    System.out.println("Hello wkwkw sedih kenapa vscodenya error");
   }
 }
